a7b013da9c7279d230570f04502244b8a7391312
5703 - Alan Joseph <5703alj@manorama.net>
2024-03-12 11:30:01 +0530

    mapping added

--- src/app/components/card.jsx
+++ src/app/components/card.jsx
@@ -3,25 +3,20 @@ import BookmarkBorderIcon from "@mui/icons-material/BookmarkBorder";
 import ShareIcon from "@mui/icons-material/Share";
 import WhatsAppIcon from "@mui/icons-material/WhatsApp";
 import KeyboardDoubleArrowRightIcon from '@mui/icons-material/KeyboardDoubleArrowRight';
-function card() {
+function card({title, desc, img}) {
   return (
     <div class="max-w-sm md:max-w-xl  bg-white border  border-gray-200 rounded-lg shadow-md mb-5 last:mb-[180px]">
       <a href="#">
-        <img class="rounded-t-lg" src="/supreme-court.avif" alt="" />
+        <img class="rounded-t-lg" src={img} alt="" />
       </a>
       <div class="p-5">
         <p className="text-sm text-[#333994] font-semibold mb-2">TOP NEWS</p>
         <h5 class="mb-2 text-xl font-bold tracking-tight  text-gray-900 ">
-          തിരഞ്ഞെടുപ്പ് കടപ്പത്രം: വിധി വന്ന് 26 ദിവസമായിട്ടും എന്തു
-          ചെയ്തുവെന്ന് സുപ്രീംകോടതി
+          {title}
         </h5>
 
         <p class="mb-3 font-normal text-[gray-700]  ">
-          ന്യൂഡൽഹി . തിരഞ്ഞെടുപ്പു കടപ്പത്രം വഴി 2019 മുതൽ രാഷ്ട്രീയ
-          പാർട്ടികൾക്കു ലഭിച്ച സംഭാവനയുടെ വിശദാംശങ്ങൾ പരസ്യപ്പെടുത്താൻ സാവകാശം
-          ആവശ്യപ്പെട്ട് എസ്ബിഐ സമർപ്പിച്ച ഹർജി സുപ്രീം കോടതി പരിഗണിക്കുന്നു.
-          ചീഫ് ജസ്റ്റിസ് ഡി.വൈ ചന്ദ്രചൂഡ് അധ്യക്ഷനായ അഞ്ചംഗ ബെഞ്ചാണ് വാദം
-          കേൾക്കുന്നത്...
+          {desc}
         </p>
         <hr />
         <div className="flex mt-3">
--- src/app/page.js
+++ src/app/page.js
@@ -1,21 +1,24 @@
+"use client";
 import Image from "next/image";
 import Card from "./components/card";
 import MoreVertIcon from "@mui/icons-material/MoreVert";
-
+import Data from "./data/data.json";
+import { useEffect } from "react";
 export default function Home() {
+  useEffect(() => {
+    console.log(Data);
+  });
   return (
     <div className="flex flex-col  items-center">
-     
       <div className="flex flex-col">
-         <div className="flex ">
-        <h2 className="font-extrabold  py-4">
-          
-          <MoreVertIcon className="text-red-600" /> QUICK READ
-        </h2>
-      </div>
-      <Card />
-        <Card />
-        <Card />
+        <div className="flex ">
+          <h2 className="font-extrabold  py-4">
+            <MoreVertIcon className="text-red-600" /> QUICK READ
+          </h2>
+        </div>
+        {Data.map((item) => (
+          <Card title={item.title} desc={item.desc} img={item.img} />
+        ))}
       </div>
       <button className="bg-red-600 text-white px-5 py-3 rounded-full font-normal fixed bottom-[100px] left-1/2 transform -translate-x-1/2">
         EXIT QUICK READ
